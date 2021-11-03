4c721ba0eda42d2a40aa354f009463ccce856313
Lucine BALAS <luciolle24@gmail.com>
2021-11-03 10:47:12 +0100

    Added referrences to the best book ever

--- logo/secret_logo2.c
+++ logo/secret_logo2.c
@@ -11,9 +11,21 @@ void lucio24(void)
 {
     my_putstr("\nHere's my LinkedIn if you want to check me out...");
     my_putstr("\nhttps://www.linkedin.com/in/lucinebalas/\n\n");
-    my_putstr("Un mur est un ami versatile. S'il surveille tes arrières, ");
-    my_putstr("il verrouille ton espace. Seul le sot se fie au mur.\n");
+    my_putstr("“Un mur est un ami versatile.\n S'il surveille tes arrières, ");
+    my_putstr("il verrouille ton espace.\n Seul le sot se fie au mur.”\n\n");
     my_putstr("~Pierre Boterro\n\n");
+    my_putstr("“- Devenir la meilleure au monde ne te ");
+    my_putstr("semble pas un objectif suffisant ?\n");
+    my_putstr("- Non, parce qu'il est accessible et marque donc une fin,");
+    my_putstr(" alors que la voie des marchombres est infinie.\n");
+    my_putstr(" Si je cherche à devenir meilleure que moi-même,");
+    my_putstr(" je ne m'arrêterai jamais.”\n\n");
+    my_putstr("~ Pierre Bottero, Ellana\n\n");
+    my_putstr("“- Que deviennent les rêves qui se brisent ?\n");
+    my_putstr(" - les rêves ne se brisent pas.\n");
+    my_putstr(" - Que deviennent les rêves qui se brisent ?\n");
+    my_putstr(" - Le terreau des rêves à venir.”\n\n");
+    my_putstr("~ Pierre Bottero, Ellana\n\n");
 }
 
 void flysland(void)
